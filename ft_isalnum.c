a8052ec2f0e5b8fcd632279694cbed4ee9b52897
randre <randre@student.s19.be>
2023-08-28 16:15:09 +0200

    some functions

---
+++ ft_isalnum.c
@@ -0,0 +1,7 @@
+int	ft_isalnum(char c)
+{
+	if (ft_isalpha || ft_isnum)
+		return (1);
+	else
+		return (0);
+}
---
+++ ft_memset.c
@@ -0,0 +1,17 @@
+#include <stddef.h>
+
+void	*ft_memset(void	*str, int c, size_t n)
+{
+	int	i;
+
+	i = 0;
+	if (str)
+	{	
+		while (i <= n)
+		{
+			((unsigned char *)str)[i] = c;
+			i++;
+		}
+	}
+	return (str);
+}
