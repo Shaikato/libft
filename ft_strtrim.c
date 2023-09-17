eac7f4bb3b2288f509839cfe99ec3c23b9d15da1
randre <randre@student.s19.be>
2023-09-17 04:48:17 +0200

    strtrim

--- ft_strtrim.c
+++ ft_strtrim.c
@@ -14,5 +14,14 @@
 
 char	*ft_strtrim(char const *s1, char const *set)
 {
-	
+	unsigned int	len;
+
+	if (!s1 || !set)
+		return (NULL);
+	while (ft_strchr(set, *s1) && *s1)
+		s1++;
+	len = (unsigned int)ft_strlen((char *)s1);
+	while (len + 1 && ft_strchr(set, s1[len]))
+		len--;
+	return(ft_substr(s1, 0, len + 1));	
 }
