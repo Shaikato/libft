3bbb0789df1aa0164de91638f3f1da35f4d6ee26
randre <randre@student.s19.be>
2023-09-19 20:09:25 +0200

    map lst

--- ft_lstmap.c
+++ ft_lstmap.c
@@ -12,7 +12,24 @@
 
 #include "libft.h"
 
-/* t_list	*ft_lstmap(t_list *lst, void *(*f)(void *), void (*del)(void))
+t_list	*ft_lstmap(t_list *lst, void *(*f)(void *), void (*del)(void))
 {
+	t_list	*lnew;
+	t_list	*new;
 
-} */
+	if (!lst || !f)
+		return (NULL);
+	lnew = NULL;
+	while (lst)
+	{
+		new = ft_lstnew(f(lst->content));
+		if (!new)
+		{
+			ft_lstclear(&lnew, del);
+			return (NULL);
+		}
+		ft_lstadd_back(&lnew, new);
+		lst = lst->next;
+	}
+	return (lnew);
+}
