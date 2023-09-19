92326a65577e929fb2246d3591b98ca011b6bf2d
randre <randre@student.s19.be>
2023-09-19 02:11:37 +0200

    substr corr

--- ft_substr.c
+++ ft_substr.c
@@ -17,10 +17,12 @@ char	*ft_substr(char const *s, unsigned int start, size_t len)
 {
 	char	*str;
 
-	str = ft_calloc((len + 1), sizeof(char));
-	if (!str)
+	if (!s)
 		return (NULL);
-	if (start > (unsigned int)ft_strlen((char *)s) - 1)
+	if (len > (unsigned int)ft_strlen((char *)s) - start + 1)
+		len = (unsigned int)ft_strlen((char *)s) - start;
+	str = ft_calloc((len + 1), sizeof(char));
+	if (start > (unsigned int)ft_strlen((char *)s) - 1 || !str)
 		return (NULL);
 	return (ft_memcpy(str, s + start, len));
 }
