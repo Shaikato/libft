6fea1e8974559c3dfcf973b2a29abd742bd42d0f
randre <randre@student.s19.be>
2023-09-19 19:54:05 +0200

    corr

--- ft_lstadd_back.c
+++ ft_lstadd_back.c
@@ -16,7 +16,8 @@ void	ft_lstadd_back(t_list **lst, t_list *new)
 {
 	if (!lst || !new)
 		return ;
-	while (*lst->next)
-		*lst = *lst->next;
-	*lst->next = new;
+	if (!*lst)
+		*lst = new;
+	else
+		ft_lstlast(*lst)->next = new;
 }
